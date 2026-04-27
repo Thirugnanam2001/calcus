92659ac8e5039f55516dfa26f5846eefef5f4e68
Thirugnanam2001 <gnanamdon2001@gmail.com>
2026-04-27 23:21:28 +0530

    first commit

---
+++ calcus_demo/src/test/java/com/example/calcus_demo/CalcusDemoApplicationTest.java
@@ -0,0 +1,26 @@
+package com.example.calcus_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        assertThat(context).isNotNull();
+    }
+
+    @Test
+    void testMainMethod() {
+        CalcusDemoApplication.main(new String[] {});
+        assertThat(true).isTrue();
+    }
+}
